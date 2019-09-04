77f68bf743f5eef83f253567bceaeec35620c7e7
Audun Hansen <audunhansen@Auduns-MBP.domain_not_set.invalid>
2019-09-04 09:08:48 +0200

    fixed domdef/fragmentation

--- domdef.py
+++ domdef.py
@@ -63,7 +63,7 @@ def build_local_domain_index_matrix(fragment, d, distance_cutoff = 10.0):
 
 def atomic_fragmentation(p, d, distance_cutoff= 1.0):
     """
-    Split configuration space into atomic fragments
+    Split configuration space into atomic/proximal fragments
     """
 
     d_occ_ref = d.cget([0,0,0])[:p.get_nocc(), :p.get_nocc()] #inter-occupied distances in the refcell
@@ -71,9 +71,19 @@ def atomic_fragmentation(p, d, distance_cutoff= 1.0):
     occ_processed = []
     center_fragments = []
     for i in occ_indices:
+        #print("Processing orbital ",i)
         if i not in occ_processed:
-            occ_processed += [i]
+
+            
+            #print(i, " added to the set:", occ_processed)
             proximal_occupied_indices = np.argwhere(d_occ_ref[ i,:]<distance_cutoff)[:,0]
+            new_fragment = []
+            for j in proximal_occupied_indices:
+                if j not in occ_processed:
+                    new_fragment.append(j)
+            center_fragments.append(new_fragment)
+            occ_processed += [i]
             occ_processed += list(proximal_occupied_indices)
-            center_fragments.append(proximal_occupied_indices)
+            #print(proximal_occupied_indices, " added to the set:", occ_processed)
+            #print("Center fragments:", center_fragments)
     return center_fragments
